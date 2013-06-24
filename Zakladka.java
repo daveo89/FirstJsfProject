8c559ea5b9682ac3346ba1883b2bc08b047e66ec
Dawid <kre.daw@gmail.com>
2013-06-24 16:52:45 +0200

    Update Zakladka.java

--- Zakladka.java
+++ Zakladka.java
@@ -2,13 +2,29 @@ package com.beans;
 
 import javax.faces.bean.ManagedBean;
 
+import org.primefaces.component.tabview.Tab;
+
 @ManagedBean
 public class Zakladka {
 
-  private int id;
+	private int id;
+	private String aktywnaZakladka;
+	private Tab tab;
 	
-	public Zakladka(){}
+	public Zakladka(){
+		
+	}
 	
+	public String getAktywnaZakladka() {
+		tab = new Tab();
+		aktywnaZakladka = tab.getTitle();
+		return aktywnaZakladka;
+	}
+
+	public void setAktywnaZakladka(String aktywnaZakladka) {
+		this.aktywnaZakladka = aktywnaZakladka;
+	}
+
 	public Zakladka(int id) {
 		this.id = id;
 	}
