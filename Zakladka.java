efd2daedf8eb4565e52355050f82891a5f534323
Dawid <kre.daw@gmail.com>
2013-06-25 02:04:38 +0300

    Update Zakladka.java

--- Zakladka.java
+++ Zakladka.java
@@ -1,32 +1,33 @@
 package com.beans;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 import org.primefaces.component.tabview.Tab;
+import org.primefaces.event.TabChangeEvent;
+
 
 @ManagedBean
 public class Zakladka {
 
 	private int id;
-	private String aktywnaZakladka;
-	private Tab tab;
+	private String aktualneId;
 	
-	public Zakladka(){
-		
-	}
+	public Zakladka(){}
 	
-	public String getAktywnaZakladka() {
-		tab = new Tab();
-		aktywnaZakladka = tab.getTitle();
-		return aktywnaZakladka;
-	}
-
-	public void setAktywnaZakladka(String aktywnaZakladka) {
-		this.aktywnaZakladka = aktywnaZakladka;
-	}
-
 	public Zakladka(int id) {
 		this.id = id;
+	}
+	
+	
+	public String getAktualneId() {
+		return aktualneId;
+	}
+
+	public void setAktualneId(String aktualneId) {
+		this.aktualneId = aktualneId;
 	}
 
 	public int getId() {
@@ -37,5 +38,12 @@ public class Zakladka {
 		this.id = id;
 	}
 	
+	public void akcjaId(ActionEvent event) {
+		aktualneId = "Test";	
+	}
+	
+	public void onChange(TabChangeEvent event) {
+		aktualneId = "Test";
+	}
 	
 }
